423e7f0e5fe0793b0e33a6bdb575f22e7bc645b8
taydrey <taydrey@users.noreply.github.com>
2020-01-27 16:45:41 +0100

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
 const router = express.Router();
+const { Client } = require('pg');
 
-router.get('/', (req, res) => {
-    res.redirect('https://documenter.getpostman.com/view/6617942/SW7dURNT');
+router.get('/', async (req, res) => {
+    const client = new Client();
+    await client.connect();
+
+    const queryEmail = 'select * from users where userid=$1';
+    const value = [1];
+
+    client.query(queryEmail, value)
+        .then((resultdata) => res.status(200).json({
+            status: 'success',
+            data: resultdata,
+        }))
+        .catch((err) => res.status(500).json({
+            status: 'error',
+            err,
+        }));
+    /* res.redirect('https://documenter.getpostman.com/view/6617942/SW7dURNT'); */
 });
 
 module.exports = router;
