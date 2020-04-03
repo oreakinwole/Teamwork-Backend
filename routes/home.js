4458d423842651429700e83090e78c6fb299a17e
taydrey <taydrey@users.noreply.github.com>
2020-04-03 06:32:14 +0100

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,10 @@ const router = express.Router();
 // const { Client } = require('pg');
 
 router.get('/', async (req, res) => {
-   /*  const client = new Client();
+    /*  const client = new Client();
     await client.connect(); */
 
     res.redirect('https://documenter.getpostman.com/view/6617942/SW7dURNT');
-})
+});
 
 module.exports = router;
