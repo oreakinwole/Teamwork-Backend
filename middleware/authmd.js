6a94150d0491b6d0056baf39736fb19dbdf05bb1
taydrey <taydrey@users.noreply.github.com>
2020-04-03 22:55:11 +0100

    Update authmd.js

--- middleware/authmd.js
+++ middleware/authmd.js
@@ -4,15 +4,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const token = req.header('teamworkToken');
-    if (!token) {
-        res.status(400).send('No Token provided.');
-        return;
-    }
-    try {
-        const headToken = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = headToken;
-        next();
-    } catch (ex) {
-        res.status(400).send('Invalid Token provided');
+    if (!token) res.status(400).send('No Token provided.');
+    if (token) {
+        try {
+            const headToken = jwt.verify(token, process.env.SECRET_KEY);
+            req.user = headToken;
+            next();
+        } catch (ex) {
+            res.status(400).send('Invalid Token provided');
+        }
     }
 };
